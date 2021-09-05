9decefc9ca252544dd81d0f5b96fef0c209f71a3
Kryštof <48121710+krystofex@users.noreply.github.com>
2021-09-05 09:13:22 +0200

    timer

---
+++ src/debug/timer.h
@@ -0,0 +1,32 @@
+#include <chrono>
+#include <iostream>
+
+class Timer
+{
+public:
+    Timer()
+    {
+        m_StartTimepoint = std::chrono::high_resolution_clock::now();
+    }
+
+    ~Timer()
+    {
+        Stop();
+    }
+
+    void Stop()
+    {
+        auto endTimepoint = std::chrono::high_resolution_clock::now();
+
+        auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
+        auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+
+        auto duration = end - start;
+        double ms = duration * 0.001;
+
+        std::cout << "File converted in \033[1;35m" << ms << "ms / " << duration << "us\n\033[0m";
+    }
+
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
+};
