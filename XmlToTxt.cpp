80d7485efa79406ad983a123049249e0b2c90203
Kryštof <48121710+krystofex@users.noreply.github.com>
2021-09-06 21:40:49 +0200

    xtt prefix

--- XmlToTxt.cpp
+++ XmlToTxt.cpp
@@ -32,37 +32,53 @@ int main()
         const int numberOfElementsInInput = std::distance(set.begin(), set.end());
 
         outputTxt += set.attribute("setID").value();
-
         for (int ffseq = 1; ffseq < numberOfElementsInSet + 1; ffseq++)
         {
-            if (ffseq > numberOfElementsInInput)
+
+            if (ffseq > numberOfElementsInInput + 1)
                 break;
 
             const auto name = currentDefinitionSet.find_child_by_attribute("ffSeq", std::to_string(ffseq).c_str()).attribute("name").value();
             const pugi::xml_node currentDefinitionElement = currentDefinitionSet.find_child_by_attribute("ffSeq", std::to_string(ffseq).c_str());
 
-            if (set.child(name).text() != 0)
+            if (set.child(name) == 0)
+                outputTxt += "/-";
+            else if (set.child(name).text() != 0)
             {
                 outputTxt += "/";
                 outputTxt += set.child(name).text().get();
             }
             else if (currentDefinitionElement.child("Choice") != 0)
             {
-                std::cout << "Choice\n";
+                std::string chooseBy = currentDefinitionElement.attribute("choice").value();
+                if (chooseBy == "prefix")
+                {
+                    std::string prefix = currentDefinitionElement.child("Choice").find_child_by_attribute("name", set.child(name).first_child().name()).attribute("prefix").value();
+                    outputTxt += "/" + prefix;
+                    if (prefix.length() > 0)
+                        outputTxt += ":";
+                    outputTxt += set.child(name).first_child().text().get();
+                }
+                else
+                {
+                    outputTxt += "/";
+                    for (const pugi::xml_node &sequenceElement : set.child(name))
+                        // here disappears " "
+                        outputTxt += sequenceElement.text().get();
+                }
             }
             else if (currentDefinitionElement.child("Sequence") != 0)
             {
                 outputTxt += "/";
                 for (const pugi::xml_node &sequenceElement : set.child(name))
+                    // here disappears " "
                     outputTxt += sequenceElement.text().get();
             }
-            else if (set.child(name) == 0)
-                outputTxt += "/-";
         }
 
         outputTxt += "//\n";
     }
 
-    std::cout << outputTxt << std::endl;
+    // std::cout << outputTxt << std::endl;
     SaveTxt(outputTxt, OUTPUT_TXT_PATH);
 }
