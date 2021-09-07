1b169612fea5548de29164dc402cf33194ae00ed
Kryštof <48121710+krystofex@users.noreply.github.com>
2021-09-07 04:26:24 +0200

    compatible with CRLF line ending input

--- src/files.h
+++ src/files.h
@@ -1,6 +1,27 @@
 #include <sstream>
 #include <fstream>
 
+void ReplaceAll(std::string &source, const std::string &from, const std::string &to)
+{
+    std::string newString;
+    newString.reserve(source.length()); // avoids a few memory allocations
+
+    std::string::size_type lastPos = 0;
+    std::string::size_type findPos;
+
+    while (std::string::npos != (findPos = source.find(from, lastPos)))
+    {
+        newString.append(source, lastPos, findPos - lastPos);
+        newString += to;
+        lastPos = findPos + from.length();
+    }
+
+    // Care for the rest after last occurrence
+    newString += source.substr(lastPos);
+
+    source.swap(newString);
+}
+
 std::string LoadTxtFile(const std::string path)
 {
     std::ifstream f(path);
@@ -11,6 +32,7 @@ std::string LoadTxtFile(const std::string path)
         os << f.rdbuf(); // reading data
         text = os.str();
     }
+    ReplaceAll(text, "\r\n", "\n"); // convert line endings from CLRF to LF
     return text;
 };
 
